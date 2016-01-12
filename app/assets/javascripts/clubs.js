4bdff664c58c47ef3edc7d2bf45c7053a3a8db7b
Bookis Smuin <bookis.smuin@gmail.com>
2016-01-12 09:15:53 -0800

    update clubs

--- app/assets/javascripts/clubs.js
+++ app/assets/javascripts/clubs.js
@@ -1,7 +1,4 @@
 $(function() {
   $(".choose").click(function(event) {
-    // event.preventDefault();
-    // select the (grand)parent div of `this` with a class of club
-    // add a class of `chosen` to that div
   });
 });
