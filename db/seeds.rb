e39bcce5d65a1f38b75fd4465bb17e28f576c3a2
Bookis Smuin <bookis.smuin@gmail.com>
2016-01-12 09:02:03 -0800

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-player = Player.create(name: "Noon Dude")
 
-club = Club.create(name: "Waterfall City")
-club_2 = Club.create(name: "Whatever City", category: "Cool Place", cool_factor: "HIGH")
+player = Player.create(name: Faker::Name.name)
+
+club = Club.create(name: Faker::App.name, category: Faker::Hacker.abbreviation, cool_factor: %w(High Medium Low).sample)
+club_2 = Club.create(name: Faker::App.name, category: Faker::Hacker.abbreviation, cool_factor: %w(High Medium Low).sample)
 
 player.clubs << club
 player.clubs << club_2
